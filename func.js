4a27a379f946d17e58e537c4f3c8e08acfe2f546
18309454243 <35165301+18309454243@users.noreply.github.com>
2018-03-24 17:01:16 +0800

    Update func.js
    完善 arr.del

--- func.js
+++ func.js
@@ -45,25 +45,62 @@ lis // ['first','php','js','ruby']
 
 /****************************************************************************************/
 
-// 说明:为Array原型对象添加del方法
-// 使用:arr.del(index);
-// index int 要删除元素的索引位置 支持负数,表示倒数
-
-// 函数
+/**
+ * 根据索引位置删除数组的一个元素
+ * @param array    arr 数组
+ * @param index    索引位置
+ * @return mixed   删除成功返回删除的值,失败返回false
+*/
 function del(arr,index)
 {
-    
-   // 返回删除的元素 
+   // 返回删除的元素
+   if(arr[index] === undefined && index>-1) return false;
    return arr.splice(index,1).pop();
-  
+
 }
 
-// 添加到Array 原型对象
+// 添加到数组原型对象上
+/**
+ * 根据索引位置删除数组的一个元素
+ * @param index    索引位置
+ * @return mixed   删除成功返回删除的值,失败返回false
+*/
 Array.prototype.del = function (index)
 {
-   return this.splice(index,1);
-  
+   // 返回删除的元素
+   if(this[index] === undefined && index>-1) return false;
+   return this.splice(index,1).pop();
+
 }
+
+// 例:删除第一个元素'php'
+var lis = ['php','js','py'];
+var res = del(lis,0);
+console.log(res); // php
+console.log(lis); // ['js','py']
+
+// 例:删除第二个元素 'js'
+var lis = ['php','js','py'];
+var res = del(lis,1);
+console.log(res); // js
+console.log(lis); // ['php','py']
+
+// 例:删除最后一个元素 'py'
+var lis = ['php','js','py'];
+var res = del(lis,-1);
+console.log(res); // py
+console.log(lis); // ['php','js']
+
+// 例:删除一个不存在的元素 返回false
+var lis = ['php','js','py'];
+var res = del(lis,11);
+console.log(res); // false
+console.log(lis); // ['php','js','py']
+
+/*********************************************************************************/
+
+
+
 
 
 
