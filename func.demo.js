fc4bf1fd269a411f84188fafc661afd5ec50a0b5
18309454243 <35165301+18309454243@users.noreply.github.com>
2018-03-24 20:41:07 +0800

    Update func.demo.js
    update

--- func.demo.js
+++ func.demo.js
@@ -1,12 +1,52 @@
-// 在数组中指定索引位置插入元素
-// arr.splice(index,length,insertVal)
-// 例: 在索引0, 'php'前面插入'c'
-var arr = ['php','js','python']
-arr.splice(0,0,'c');
-console.log(arr); // (4) ["c", "php", "js", "python"]
+/**
+ * 元素指定索引位置删除指定数量元素,再插入新的元素,注意是前插
+ * @param int      index    索引位置,支持负数
+ * @param int      len      要删除的元素长度
+ * @param mixed    val      要插入的值
+ * @return array   删除的元素
+*/
+Array.prototype._splice = function (index,len,val){};
 
-// 说明: splice 有返回值, 返回类型 object 中的数组, 可能是空数组,或者是移出的数组['移出的元素']
-// index 索引支持负数参数, 表示倒数
+// 例:删除第一个元素,并插入'py'
+var lis = ['js','php','ruby'];
+var res = lis.splice(0,1,'py');
+console.log(res);// ["js"]
+console.log(lis);// ["py", "php", "ruby"]
+
+// 例:删除索引1的'php'
+var lis = ['js','php','ruby'];
+var res = lis.splice(1,1);
+console.log(res);// ["php"]
+console.log(lis);// ["js", "ruby"]
+
+// 例:删除'php','ruby'
+var lis = ['js','php','ruby'];
+var res = lis.splice(1,2);
+console.log(res);// ["php","ruby"]
+console.log(lis);// ["js"]
+
+// 例:删除最后一个元素, 并插入'last'
+var lis = ['js','php','ruby'];
+var res = lis.splice(-1,1,'last');
+console.log(res);// ["ruby"]
+console.log(lis);// ['js','php','last'];
+
+
+// 扩展: 删除最后一个元素,并插入'last'
+var lis = ['js','php','ruby'];
+var res = lis.pop();
+lis.push('last');
+console.log(res);// ruby
+console.log(lis);// ['js','php','last'];
+
+// 扩展: 删除第一个元素,并插入'first'
+var lis = ['js','php','ruby'];
+var res = lis.shift();
+lis.unshift('first');
+console.log(res);// js
+console.log(lis);// ['first','php','ruby'];
+
+/***************************************************************/
 
 
 
