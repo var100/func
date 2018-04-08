d5441882b3f2faf5aece669d1f0c8caad03a2fb5
18309454243 <35165301+18309454243@users.noreply.github.com>
2018-04-08 16:00:31 +0800

    Update sys.func.test.php
    strtotime

--- sys.func.test.php
+++ sys.func.test.php
@@ -1,1 +1,21 @@
 <?php
+/***************************日期函数*****************************/
+//strtotime
+//2009-03-01 00:00:00 下一天
+echo date('Y-m-d H:i:s',strtotime('next day 2009-02-28'));
+//2009-03-01 00:00:00 下一天
+echo date('Y-m-d H:i:s',strtotime('2009-02-28 +1 day'));
+//2009-03-01 00:00:00  下一天
+echo date('Y-m-d H:i:s',strtotime('2009-02-28+24 hours'));
+//2009-02-28 00:00:00 上一天
+echo date('Y-m-d H:i:s',strtotime('last day 2009-03-01'));
+
+//2009-03-03 00:00:00
+echo date('Y-m-d H:i:s',strtotime('2009-03-31 -1 month'));
+//2009-03-03 00:00:00
+echo date('Y-m-d H:i:s',strtotime('next month 2009-01-31'));
+//2009-03-01 00:00:00
+echo date('Y-m-d H:i:s',strtotime('2009-01-29 +1 month'));
+
+
+
