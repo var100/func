b9b3a75917a687ac51513808ff33a19eea9ad27c
18309454243 <35165301+18309454243@users.noreply.github.com>
2018-04-24 11:25:43 +0800

    Update test.php

--- test.php
+++ test.php
@@ -14,3 +14,144 @@ function test(...$lis){
 }
 echo test(1,2,3);//6
 
+/******************************************************************************/
+
+//写一个函数，通过递归调用将以下数组打印出来： 
+$arr = array(
+array('id'=>1, 'pid'=>0, 'text'=>'1'),
+array('id'=>2, 'pid'=>0, 'text'=> '2'),
+array('id'=>3, 'pid'=>0, 'text'=> '3'),
+array('id'=>4, 'pid'=>0, 'text'=> '4'),
+array('id'=>5, 'pid'=>0, 'text'=> '5'),
+array('id'=>6, 'pid'=>0, 'text'=> '6'),
+array('id'=>7, 'pid'=>3, 'text'=> '3.1'),
+array('id'=>8, 'pid'=>3, 'text'=> '3.2'),
+array('id'=>9, 'pid'=>3, 'text'=> '3.3'),
+array('id'=>10, 'pid'=>9, 'text'=> '3.3.1'),
+array('id'=>11, 'pid'=>9, 'text'=> '3.3.2'),
+array('id'=>12, 'pid'=>9, 'text'=> '3.3.3'), );
+//1 2 3 --------3.1 --------3.2 --------3.3 ----------------3.3.1 ----------------3.3.2 ----------------3.3.3 4 5 6
+
+function test($data,$id=0,$deep=0)
+{
+  $lis = [];
+  foreach($data as $row)
+  {
+      if($row['pid']==$id)
+      {
+          $row['deep'] = $deep;
+          $lis[] = $row;
+          $lis = array_merge($lis,test($data,$row['id'],$deep+1));
+      }
+      
+     
+        
+  }
+   
+   return $lis;
+    
+}
+echo '<pre>';
+print_r(test($arr));
+/*
+Array
+(
+    [0] => Array
+        (
+            [id] => 1
+            [pid] => 0
+            [text] => 1
+            [deep] => 0
+        )
+
+    [1] => Array
+        (
+            [id] => 2
+            [pid] => 0
+            [text] => 2
+            [deep] => 0
+        )
+
+    [2] => Array
+        (
+            [id] => 3
+            [pid] => 0
+            [text] => 3
+            [deep] => 0
+        )
+
+    [3] => Array
+        (
+            [id] => 7
+            [pid] => 3
+            [text] => 3.1
+            [deep] => 1
+        )
+
+    [4] => Array
+        (
+            [id] => 8
+            [pid] => 3
+            [text] => 3.2
+            [deep] => 1
+        )
+
+    [5] => Array
+        (
+            [id] => 9
+            [pid] => 3
+            [text] => 3.3
+            [deep] => 1
+        )
+
+    [6] => Array
+        (
+            [id] => 10
+            [pid] => 9
+            [text] => 3.3.1
+            [deep] => 2
+        )
+
+    [7] => Array
+        (
+            [id] => 11
+            [pid] => 9
+            [text] => 3.3.2
+            [deep] => 2
+        )
+
+    [8] => Array
+        (
+            [id] => 12
+            [pid] => 9
+            [text] => 3.3.3
+            [deep] => 2
+        )
+
+    [9] => Array
+        (
+            [id] => 4
+            [pid] => 0
+            [text] => 4
+            [deep] => 0
+        )
+
+    [10] => Array
+        (
+            [id] => 5
+            [pid] => 0
+            [text] => 5
+            [deep] => 0
+        )
+
+    [11] => Array
+        (
+            [id] => 6
+            [pid] => 0
+            [text] => 6
+            [deep] => 0
+        )
+
+)
+
+*/
