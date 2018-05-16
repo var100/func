58c5f6f76f209b1d1a9339a55f49cf69cf2565db
18309454243 <35165301+18309454243@users.noreply.github.com>
2018-05-16 15:08:09 +0800

    Update code.php

--- code.php
+++ code.php
@@ -13,4 +13,71 @@ echo $a . " " . $b;
 $i = '11';
 printf("%d\n",printf("%d",printf("%d",$i)));
 ?>
+/**************************************************************/
+<?php
+class Person{
+    private $name;
+    private $age;
+    public function __construct($name,$age){
+        $this->name = $name;
+        $this->age = $age;
+    }
+    public function __destruct()
+    {
+       echo $this->name.'<br>';
+    }
+    
+    
+    
+}
+$p = new Person('zs',28);
+$p = new Person('ls',28);
+$p = new Person('ww',28);
+?>
+
+/**************************************************************/
+<?php
+class Person{
+    private $name;
+    private $age;
+    public function __construct($name,$age){
+        $this->name = $name;
+        $this->age = $age;
+    }
+    public function __destruct()
+    {
+       echo $this->name.'<br>';
+    }
+    
+    
+    
+}
+$p1 = new Person('zs',28);
+$p2 = new Person('ls',28);
+$p3 = new Person('ww',28);
+?>
+/**************************************************************/
+<?php
+class Person{
+    private $name;
+    private $age;
+    public function __construct($name,$age){
+        $this->name = $name;
+        $this->age = $age;
+    }
+    public function __destruct()
+    {
+       echo $this->name.'<br>';
+    }
+    
+    
+    
+}
+$p1 = new Person('zs',28);
+$p2 = new Person('ls',28);
+$p2 = 123;
+$p3 = new Person('ww',28);
+?>
+
+
 
